e1c2f75561995d9563175e74ff6c469f42bd6296
olohiperbolo <aleksanderopszala@gmail.com>
2024-05-25 17:36:06 +0200

    dodanie karuzeli itp

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Funkcja obsługująca kliknięcie przycisku "Dołącz" w modalu konta, który otwiera modal rejestracji
     const joinButton = document.querySelector('#accountModal .btn-outline-secondary');
 
     if (joinButton) {
@@ -11,6 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    // Inicjalizacja sekcji produktów na podstawie ich kategorii
     const sections = {
         kobieta: document.getElementById('kobieta'),
         mezczyzna: document.getElementById('mezczyzna'),
@@ -19,31 +21,33 @@ document.addEventListener('DOMContentLoaded', () => {
         ofertaspecjalna: document.getElementById('ofertaspecjalna')
     };
 
+    // Pobranie produktów z pliku JSON i wyświetlenie ich według kategorii
     fetch('products.json')
         .then(response => response.json())
         .then(products => {
             displayProductsByCategory(products);
         });
 
+    // Funkcja do wyświetlania produktów w odpowiednich sekcjach według kategorii
     const displayProductsByCategory = (products) => {
         products.forEach(product => {
             let newProduct = document.createElement('div');
             newProduct.classList.add('col-md-3');
             newProduct.innerHTML = `
             <div class="card no-border no-border-radius">
-                <a href="produkt.html" target="_blank" style="text-decoration: none; color: black">
-                    <img src="${product.image}" class="card-img-top" alt="${product.name}">
+                <a href="produkt.html?productId=${product.id}" target="_blank" style="text-decoration: none; color: black">
+                    <img src="${product.images[0]}" class="card-img-top" alt="${product.name}">
                     <div class="card-body">
                         <h5 class="card-title">${product.name}</h5>
                         <p class="card-text">${product.price} PLN</p>
-                        <button class="addCart btn btn-outline-secondary" data-id="${product.id}" data-name="${product.name}" data-price="${product.price}" data-image="${product.image}">
+                        <button class="addCart btn btn-outline-secondary" data-id="${product.id}" data-name="${product.name}" data-price="${product.price}" data-image="${product.images[0]}">
                             <i class="fas fa-shopping-cart"></i>
                         </button>
-                        <button class="addFavorite btn btn-outline-secondary" data-id="${product.id}" data-name="${product.name}" data-price="${product.price}" data-image="${product.image}">
+                        <button class="addFavorite btn btn-outline-secondary" data-id="${product.id}" data-name="${product.name}" data-price="${product.price}" data-image="${product.images[0]}">
                             <i class="fas fa-heart"></i>
                         </button>
                     </div>
-                </a> <!-- Zamknięcie linku -->
+                </a>
             </div>
         `;
 
@@ -57,6 +61,7 @@ document.addEventListener('DOMContentLoaded', () => {
         setupFavoriteButtons();
     };
 
+    // Funkcja ustawiająca obsługę przycisków dodawania produktów do koszyka
     const setupCartButtons = () => {
         const addCartButtons = document.querySelectorAll('.addCart');
 
@@ -78,6 +83,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     };
 
+    // Funkcja ustawiająca obsługę przycisków dodawania produktów do ulubionych
     const setupFavoriteButtons = () => {
         const addFavoriteButtons = document.querySelectorAll('.addFavorite');
 
@@ -99,6 +105,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     };
 
+    // Funkcja dodająca produkt do koszyka w localStorage
     const addToCart = (product) => {
         let cart = JSON.parse(localStorage.getItem('cart')) || [];
         const productExists = cart.some(item => item.id === product.id);
@@ -114,6 +121,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 
+    // Funkcja dodająca produkt do ulubionych w localStorage
     const addToFavorites = (product) => {
         let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
         const favoriteExists = favorites.some(item => item.id === product.id);
@@ -129,11 +137,12 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 
+    // Funkcja wyświetlająca produkty w koszyku
     const displayCartItems = () => {
         const cartItemsContainer = document.getElementById('cartItems');
         const cart = JSON.parse(localStorage.getItem('cart')) || [];
 
-        // Clear the container
+        // Czyszczenie zawartości kontenera
         cartItemsContainer.innerHTML = '';
 
         cart.forEach(product => {
@@ -155,6 +164,7 @@ document.addEventListener('DOMContentLoaded', () => {
         updateTotalPrice();
     };
 
+    // Funkcja ustawiająca obsługę przycisków usuwania produktów z koszyka
     const setupRemoveButtons = () => {
         const removeButtons = document.querySelectorAll('.remove-btn');
 
@@ -167,6 +177,7 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     };
 
+    // Funkcja usuwająca produkt z koszyka
     const removeFromCart = (productId) => {
         let cart = JSON.parse(localStorage.getItem('cart')) || [];
         cart = cart.filter(product => product.id !== productId);
@@ -174,18 +185,20 @@ document.addEventListener('DOMContentLoaded', () => {
         updateTotalPrice();
     };
 
+    // Funkcja aktualizująca łączną cenę produktów w koszyku
     const updateTotalPrice = () => {
         const cart = JSON.parse(localStorage.getItem('cart')) || [];
         const totalPrice = cart.reduce((sum, product) => sum + product.price, 0);
         document.getElementById('total-price').textContent = totalPrice.toFixed(2);
     };
 
+    // Funkcja wyświetlająca ulubione produkty
     const displayFavouriteItems = () => {
         const favouriteItemsContainer = document.getElementById('favouriteItems');
         const favourites = JSON.parse(localStorage.getItem('favorites')) || [];
         console.log('Ulubione produkty:', favourites);
 
-        // Clear the container
+        // Czyszczenie zawartości kontenera
         favouriteItemsContainer.innerHTML = '';
 
         favourites.forEach(product => {
@@ -209,3 +222,38 @@ document.addEventListener('DOMContentLoaded', () => {
     displayCartItems();
     displayFavouriteItems();
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const productId = urlParams.get('productId');
+
+    if (productId) {
+        fetch('products.json')
+            .then(response => response.json())
+            .then(data => {
+                const product = data.find(item => item.id == productId);
+                if (product) {
+                    const carouselInner = document.getElementById('carousel-inner');
+                    const images = product.images;
+                    carouselInner.innerHTML = ''; // Czyszczenie istniejących elementów
+
+                    images.forEach((imageSrc, index) => {
+                        const div = document.createElement('div');
+                        div.className = index === 0 ? 'carousel-item active' : 'carousel-item';
+                        const img = document.createElement('img');
+                        img.className = 'd-block w-100';
+                        img.src = imageSrc;
+                        div.appendChild(img);
+                        carouselInner.appendChild(div);
+                    });
+
+                    // Re-inicjalizacja karuzeli po dodaniu nowych elementów
+                    $('.carousel').carousel();
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching product data:', error);
+            });
+    }
+});
+
